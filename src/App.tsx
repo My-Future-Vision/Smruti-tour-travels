7623c0afb9cd2d64e9df1de539a47320ac492089
Umesh <umesh.raju021@gmail.com>
2026-02-14 18:56:21 +0530

    updated repo

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <Router basename="/Smruti-tour-travels">
+    <Router>
       <ScrollToTop />
       <Layout>
         <Routes>
